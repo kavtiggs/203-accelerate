9c625d93fc47ec06cfe99d387a0c9e8b3320ba3e
kavtiggs <kavonna.tiggs@gmail.com>
2021-01-11 08:36:15 -0500

    Added advanced custom fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,8 +16,46 @@ get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+            <?php while ( have_posts() ) : the_post();
+                $services = get_field('services');
+                $link = get_field('site_link');
+                $client = get_field('client');
+                $image_1 = get_field('image_1');
+                $image_2 = get_field('image_2');
+                $image_3 = get_field('image_3');
+            ?>
+            
+            <article class="case-study">
+                <?php $size = "full";
+                ?>
+                <aside class="case-study-sidebar">
+                    <h2><?php the_title(); ?></h2>
+                    <h4><?php echo $services; ?></h4>
+                    <h4>Client: <?php echo $client; ?></h4>
+
+                     <?php the_content(); ?>
+                        <p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site ›</a></p>
+                </aside>
+       
+                <div class="case-study-images">
+                    <?php if($image_1) {
+                        echo wp_get_attachment_image( $image_1, $size );
+                        } 
+                    ?>
+                    
+                    <?php if(image_2) { 
+                        echo wp_get_attachment_image( $image_2, $size );
+                        } 
+                    ?>
+
+                    <?php if(image_3) { 
+                        echo wp_get_attachment_image( $image_3, $size );
+                        } 
+                    ?>
+                </div>
+
+               
+            </article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->	
 
